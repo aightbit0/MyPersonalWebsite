d25f33883d5b0c2df84017122321ac602e8d1ca8
aightbit0 <svenlatka@web.de>
2022-05-29 17:47:54 +0200

    new image and fixed statusbar at top

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ window.addEventListener("scroll", (event) => {
 
     var scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
     let faktor = (scrollMaxY/100);
-
     let scroll = this.scrollY;
-    let percentage = scroll/faktor;
+    let percentage = parseInt(scroll/faktor);
     if(percentage >= 0 && percentage <=100){
-        document.getElementsByClassName("status")[0].style.width = percentage+"%";
+        if(percentage >= 98){
+           percentage = 100;
+        }
+        document.getElementsByClassName("status")[0].style.width = 
+        percentage+"%";
     }
 });
